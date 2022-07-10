24ae365e1ea3a12dc73e923c5b17bffdf553fc1a
FeelGood1337 <sidodji1337@yandex.ru>
2022-07-11 00:01:34 +0300

    Apply suggestions from code review
    Co-authored-by: Evgeniy Gorodilov <gorodilov.box@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // URLS
 export const API_URL = 'https://ya-praktikum.tech/api/v2';
-export const AVATAR_URL = 'https://ya-praktikum.tech/api/v2/resources';
+export const AVATAR_URL = `${API_URL}/resources`;
 
 export const SUBJECT_TYPES = {
     LOGOUT: 'LOGOUT',
