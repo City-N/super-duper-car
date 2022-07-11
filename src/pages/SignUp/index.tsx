acd3a25f68283743de19cccfa6ea2db7cb8491db
FeelGood1337 <sidodji1337@yandex.ru>
2022-07-11 19:22:58 +0300

    Update src/pages/SignUp/index.tsx
    Co-authored-by: Evgeniy Gorodilov <gorodilov.box@gmail.com>

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -15,7 +15,7 @@ import colors from 'colors';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Link as RouterLink } from 'react-router-dom';
 import theme from 'theme';
-import { ISignUp, signup } from 'API/auth-api';
+import type { ISignUp, signup } from 'API/auth-api';
 
 const SignUpPage = () => {
     const formik = useFormik({
