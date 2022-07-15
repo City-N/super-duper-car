f33aef01adfeeaada78f6c363aa8954a546c246a
Sergey Vlasov <sidodji1337@yandex.ru>
2022-07-15 13:23:02 +0300

    typing fix

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -42,6 +42,7 @@ export const App = () => {
                 }
             })
             .then(() => redirectoToHomePage());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [data.id]);
 
     return (
