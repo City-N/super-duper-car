9315acb5e2c7b2025086a239f6e354dc17ab8191
matroskin92 <dev@matro.site>
2022-07-16 11:21:13 +0400

    Убрал не используеме переменные

--- src/components/leader-table.tsx
+++ src/components/leader-table.tsx
@@ -42,45 +42,6 @@ const rows = [
     createData('https://via.placeholder.com/64/0000FF/808080?Text=Examle', 'Name', 'Nickname', 231),
 ];
 
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-type Order = 'asc' | 'desc';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function getComparator<Key extends keyof any>(
-    order: Order,
-    orderBy: Key,
-): (
-    a: { [key in Key]: number | string },
-    b: { [key in Key]: number | string },
-) => number {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort<T>(array: ReadonlyArray<T>, comparator: (a: T, b: T) => number) {
-    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map(el => el[0]);
-}
-
 interface HeadCell {
     disablePadding: boolean;
     id: keyof Data;
@@ -114,7 +75,6 @@ const headCells: ReadonlyArray<HeadCell> = [
         label: 'Рекорд',
     },
 ];
-
 
 function EnhancedTableHead() {
     return (
