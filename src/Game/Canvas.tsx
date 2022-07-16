b193a66086f2b154e454a96fd07d5f01644783dc
Sergey Vlasov <sidodji1337@yandex.ru>
2022-07-16 18:54:54 +0300

    работы с фоном

--- src/Game/Canvas.tsx
+++ src/Game/Canvas.tsx
@@ -3,18 +3,54 @@ import colors from 'colors';
 
 import style from './canvas.module.css';
 import Sky from '../img/game_sprites/city/Sky.png';
+import Road from '../img/game_sprites/city/Road.png';
+import Back from '../img/game_sprites/city/back.png';
 
 const CanvasComponent = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null) as MutableRefObject<HTMLCanvasElement>;
-    const image = new Image();
+
+    const imageBG = new Image();
+    const imageRoad = new Image();
+    const imageBack = new Image();
+
+    const renderSky = (ctx: CanvasRenderingContext2D) => {
+        imageBG.src = Sky;
+        imageBG.style.width = '736px';
+        imageBG.style.height = '472px';
+
+        imageBG.onload = () => {
+            ctx?.drawImage(imageBG, 0, 0, 300, 150);
+            ctx.imageSmoothingEnabled = false;
+        };
+    };
+
+    const renderBack = (ctx: CanvasRenderingContext2D) => {
+        imageBack.src = Back;
+        imageBack.style.width = '736px';
+        imageBack.style.height = '472px';
+
+        imageBack.onload = () => {
+            ctx?.drawImage(imageBack, 0, 0, 300, 150);
+            ctx.imageSmoothingEnabled = false;
+        };
+    };
+
+    const renderRoad = (ctx: CanvasRenderingContext2D) => {
+        imageRoad.src = Road;
+        imageRoad.style.width = '736px';
+        imageRoad.style.height = '472px';
+
+        imageRoad.onload = () => {
+            ctx?.drawImage(imageRoad, 0, 0, 300, 150);
+            ctx.imageSmoothingEnabled = false;
+        };
+    };
 
     useEffect(() => {
-        const ctx = canvasRef.current.getContext('2d');
-
-        image.src = Sky;
-        image.onload = () => ctx?.drawImage(image, 0, 0, 400, 200);
-
-        // ctx.fillRect(0, 0, 100, 100);
+        const ctx = canvasRef.current.getContext('2d') as CanvasRenderingContext2D;
+        renderSky(ctx);
+        renderBack(ctx);
+        renderRoad(ctx);
     }, []);
 
     return (
