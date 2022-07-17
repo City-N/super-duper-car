6dfe50971a34b6cd5324f5c901468a8a1fd06f35
Sergey Vlasov <sidodji1337@yandex.ru>
2022-07-17 15:24:26 +0300

    наведения порядка и анимация заднего фона города

---
+++ src/Game/game_constants.ts
@@ -0,0 +1,27 @@
+const CANVAS_X_SIZE = 736;
+const CANVAS_Y_SIZE = 472;
+
+const SKY_POS = {
+    x: 0,
+    y: 0,
+    dx: 3,
+    dy: -2,
+};
+
+const BACK_CITY_POS = {
+    x: 0,
+    y: 0,
+    dx: 1,
+    dy: -2,
+};
+
+const ROAD_POS = {
+    x: 0,
+    y: 0,
+    dx: 3,
+    dy: -2,
+};
+
+export {
+    CANVAS_X_SIZE, CANVAS_Y_SIZE, SKY_POS, BACK_CITY_POS, ROAD_POS,
+};
