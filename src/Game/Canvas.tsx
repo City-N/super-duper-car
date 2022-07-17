4d09d30dbf34cbd54ee732173c4afded52cd47bf
Sergey Vlasov <sidodji1337@yandex.ru>
2022-07-17 13:33:50 +0300

    рефакторинг с использованием хука для канваса

--- src/Game/Canvas.tsx
+++ src/Game/Canvas.tsx
@@ -1,62 +1,19 @@
-import React, { useRef, useEffect, MutableRefObject } from 'react';
+import React from 'react';
 import colors from 'colors';
 
+import useCanvas from 'hooks/useCanvas';
 import style from './canvas.module.css';
-import Sky from '../img/game_sprites/city/Sky.png';
-import Road from '../img/game_sprites/city/Road.png';
-import Back from '../img/game_sprites/city/back.png';
+import draw from './game';
 
 const CanvasComponent = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null) as MutableRefObject<HTMLCanvasElement>;
-
-    const imageBG = new Image();
-    const imageRoad = new Image();
-    const imageBack = new Image();
-
-    const CanvasXSize = 736;
-    const CanvasYSize = 472;
-
-    const renderSky = (ctx: CanvasRenderingContext2D) => {
-        imageBG.src = Sky;
-
-        imageBG.onload = () => {
-            ctx?.drawImage(imageBG, 0, 0, CanvasXSize, CanvasYSize);
-        };
-    };
-
-    const renderBack = (ctx: CanvasRenderingContext2D) => {
-        imageBack.src = Back;
-
-        imageBack.onload = () => {
-            ctx?.drawImage(imageBack, 0, 0, CanvasXSize, CanvasYSize);
-        };
-    };
-
-    const renderRoad = (ctx: CanvasRenderingContext2D) => {
-        imageRoad.src = Road;
-
-        imageRoad.onload = () => {
-            ctx?.drawImage(imageRoad, 0, 0, CanvasXSize, CanvasYSize);
-        };
-    };
-
-    useEffect(() => {
-        const ctx = canvasRef.current.getContext('2d') as CanvasRenderingContext2D;
-        canvasRef.current.width = CanvasXSize;
-        canvasRef.current.height = CanvasYSize;
-        renderSky(ctx);
-        renderBack(ctx);
-        renderRoad(ctx);
-    }, []);
+    const canvasRef = useCanvas({ draw });
 
     return (
-        <>
-            <canvas
-                className={style.Canvas}
-                ref={canvasRef}
-                style={{ backgroundColor: colors.canvasBG }}
-            />
-        </>
+        <canvas
+            className={style.Canvas}
+            ref={canvasRef}
+            style={{ backgroundColor: colors.canvasBG }}
+        />
     );
 };
 
