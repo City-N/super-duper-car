c7ffceca0c856c09904c6d5e97a52252423aceba
Sergey Vlasov <sidodji1337@yandex.ru>
2022-07-26 17:09:42 +0300

    фикс наслоения персонажей

--- src/Game/game.ts
+++ src/Game/game.ts
@@ -12,41 +12,10 @@ import {
 } from './game_constants';
 
 const posX = 244;
+
+let enemPosX = CANVAS_X_SIZE + 20;
+let enemPosY = 220;
 let moveOn = 0;
-let lastKey = '';
-
-const throttleHeroMove = throttle(() => {
-    if (moveOn >= 1220) {
-        moveOn = 0;
-    }
-    moveOn += posX;
-}, 80, null);
-
-const drawHero = (
-    ctx: CanvasRenderingContext2D,
-) => {
-    const imagHero = new Image();
-    imagHero.src = Hero;
-
-    imagHero.onload = () => {
-        ctx.clearRect(0, 0, CANVAS_X_SIZE, CANVAS_Y_SIZE);
-        ctx.drawImage(imagHero, HERO_POS.x, HERO_POS.y);
-    };
-
-    ctx.drawImage(
-        imagHero,
-        moveOn,
-        0,
-        imagHero.width / 6,
-        imagHero.height,
-        HERO_POS.x,
-        HERO_POS.y,
-        imagHero.width / 6,
-        imagHero.height,
-    );
-    throttleHeroMove();
-};
-
 let moveOnEnemy = 0;
 
 const throttleEnemyMove = throttle(() => {
@@ -67,8 +36,6 @@ function generateRandom(min = 0, max = 1): number {
     return rand;
 }
 
-let enemPosX = 438;
-let enemPosY = 220;
 const drawEnemyFirst = (
     ctx: CanvasRenderingContext2D,
 ) => {
@@ -97,7 +64,60 @@ const drawEnemyFirst = (
         enemPosY = generateRandom() ? 260 : 220;
     }
 
-    enemPosX -= 5;
+    enemPosX -= 2;
+};
+
+const throttleHeroMove = throttle(() => {
+    if (moveOn >= 1220) {
+        moveOn = 0;
+    }
+    moveOn += posX;
+}, 80, null);
+
+const drawHero = (
+    ctx: CanvasRenderingContext2D,
+) => {
+    const imagHero = new Image();
+    imagHero.src = Hero;
+
+    imagHero.onload = () => {
+        ctx.clearRect(0, 0, CANVAS_X_SIZE, CANVAS_Y_SIZE);
+        ctx.drawImage(imagHero, HERO_POS.x, HERO_POS.y);
+    };
+
+    if (enemPosY === 260) {
+        ctx.drawImage(
+            imagHero,
+            moveOn,
+            0,
+            imagHero.width / 6,
+            imagHero.height,
+            HERO_POS.x,
+            HERO_POS.y,
+            imagHero.width / 6,
+            imagHero.height,
+        );
+        throttleHeroMove();
+        drawEnemyFirst(ctx);
+    } else {
+        drawEnemyFirst(ctx);
+        ctx.drawImage(
+            imagHero,
+            moveOn,
+            0,
+            imagHero.width / 6,
+            imagHero.height,
+            HERO_POS.x,
+            HERO_POS.y,
+            imagHero.width / 6,
+            imagHero.height,
+        );
+        throttleHeroMove();
+    }
+
+    // if (HERO_POS.y === enemPosY) {
+    //     alert('Вы проебались сээээр');
+    // }
 };
 
 let moveOnBg = 0;
@@ -133,15 +153,10 @@ document.addEventListener('keydown', (e: KeyboardEvent) => {
     switch (e.key) {
         case 'w':
             KEYS.w.pressed = true;
-            lastKey = 'w';
-            break;
-        case 'a':
-            KEYS.a.pressed = true;
-            lastKey = 'a';
+            HERO_POS.y = 220;
             break;
         case 's':
             KEYS.s.pressed = true;
-            lastKey = 's';
             HERO_POS.y = 260;
             break;
         default:
@@ -164,7 +179,6 @@ document.addEventListener('keyup', (e: KeyboardEvent) => {
 
 const draw = (ctx: CanvasRenderingContext2D) => {
     drawBG(ctx);
-    drawEnemyFirst(ctx);
     drawHero(ctx);
 };
 
