b13d8b7597362cf55cb4dda2a813398c10db80ed
Sergey Vlasov <sidodji1337@yandex.ru>
2022-07-27 17:31:43 +0300

    добавлена игра на главную

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import Header from 'components/Header';
+import CanvasComponent from 'components/Canvas/Canvas';
+import { Box } from '@mui/material';
 
-const MainPage = () => <Header />;
+const MainPage = () => <>
+    <Header />
+    <Box sx={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }}>
+        <CanvasComponent />
+    </Box>
+</>;
 
 export default MainPage;
