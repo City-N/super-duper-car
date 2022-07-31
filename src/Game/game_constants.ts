80458d0b13b5e2891361c96b4a544b7af588f3e5
Sergey Vlasov <sidodji1337@yandex.ru>
2022-07-31 14:56:43 +0300

    фикс расположения машин при столкновении и вынос значений для врага в константы

--- src/Game/game_constants.ts
+++ src/Game/game_constants.ts
@@ -5,14 +5,18 @@ const BG_POS = {
     x: 0,
     y: 0,
     dx: 0.5,
-    dy: -2,
 };
 
 const HERO_POS = {
     x: 18,
     y: 260,
     dx: 3,
-    dy: -2,
+};
+
+const ENEMY_POS = {
+    x: CANVAS_X_SIZE + 20,
+    y: 220,
+    dx: 2,
 };
 
 const KEYS = {
@@ -27,5 +31,6 @@ export {
     CANVAS_Y_SIZE,
     BG_POS,
     HERO_POS,
+    ENEMY_POS,
     KEYS,
 };
