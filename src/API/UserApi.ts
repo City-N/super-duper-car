7ff2ee0b1a2896a82ab976d2be9dfefe779620f2
Sergey Vlasov <sidodji1337@yandex.ru>
2022-08-03 18:54:43 +0300

    забыл файлы

---
+++ src/API/UserApi.ts
@@ -0,0 +1,17 @@
+import {
+    AVATAR_URL, GET_AVATAR_URL, GET_PROFILE_URL, GET_USER_URL, UPDATE_PROFILE_URL,
+} from 'constants/constants';
+import { AxiosInstance } from './AxiosInstance';
+
+const getUser = () => AxiosInstance.get(GET_USER_URL);
+
+const updateProfile = <T>(data: T) => AxiosInstance.put<string>(UPDATE_PROFILE_URL, data);
+
+const getProfile = <T>(data: T) => AxiosInstance.get<string>(`${GET_PROFILE_URL}/${data}`, data);
+
+const updateAvatar = <T>(data: T) => AxiosInstance.put<string>(AVATAR_URL, data);
+const getAvatar = <T>(data: T) => AxiosInstance.get<string>(`${GET_AVATAR_URL}/${data}`);
+
+export {
+    getUser, updateProfile, getProfile, updateAvatar, getAvatar,
+};
